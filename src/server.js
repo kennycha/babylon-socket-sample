6720fb3cea4799f3ff9a3c99cfc1c1260b6508d7
kennycha <dudqn136@naver.com>
2022-02-27 22:11:49 +0900

    feat: create socket server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import SocketIO from 'socket.io'
 import http from 'http'
 
 const app = express()
@@ -10,6 +11,22 @@ app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.redirect('/'))
 
 const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
+
+wsServer.on('connection', (socket) => {
+  socket['userName'] = 'anonymous'
+
+  socket.on('enter', (userName, roomName) => {
+    socket['userName'] = userName
+    socket.join(roomName)
+    socket.to(roomName).emit('welcome', socket.userName, roomName)
+  })
+
+  socket.on('transform', (roomName, targetId, property, value) => {
+    const updatedAt = socket.handshake.time.split(' ')[4]
+    socket.to(roomName).emit('transform', socket.userName, targetId, property, value, updatedAt)
+  })
+})
 
 const handleListen = () => {
   console.log('Listening on http://localhost:3000')
